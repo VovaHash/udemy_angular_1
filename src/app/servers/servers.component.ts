afa9539f1d48b41302fe766fb6aead0188fddc99
VovaHash <vovahash01@gmail.com>
2022-10-26 23:33:08 +0300

    two way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,21 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '.app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer:boolean = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  buttonStatus = 'Wait a little';
 
-  allowNewServer = false;
 
 
-  constructor() { 
-    setTimeout(()=>{
-      this.allowNewServer = true
-    } ,2000)
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+      this.buttonStatus = 'Add a server';
+    }, 2000);
   }
 
-  ngOnInit(): void {
-  }
 
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    if (this.serverName == '') {
+      this.serverCreationStatus =
+        'Can not create a new server without a name. Please enter a name for the server.';
+    } else {
+      this.serverCreationStatus =
+        'Server was created. Name is ' + this.serverName;
+    }
+  }
 }
-
